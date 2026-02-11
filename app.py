297a9d757c69ba65254ba8ab5f0f016e199a9b9d
niel1081610816 <niel10816albaredo3@gmail.com>
2026-02-11 19:05:50 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from openai import OpenAI
+
+client = OpenAI(api_key="sk-or-v1-44b...055", base_url="httpsw://openrouter.ai/api/v1")
+
+st.title("Consigliatore Film/Serie")
+
+titolo = st.text_input("Scrivi un film o una serie che ti è piaciuto")
+
+if st.button("Trova qualcosa di simile"):
+    if titolo:
+        risposta = client.chat.completions.create(model="opena/gpt-4o-mini",messages=[{"role": "system", "content": "sei un assistente che consiglia film o seriesimili a quello indicato, basandotisullo stesso mood e genere."},
+                                                                                      {"role": "user", "content": titolo}])
+st.writer(risposta.choices[0].message.content)
