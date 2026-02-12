ae44c2947537fd2099a8db4dc8d0fa4cae3b1232
niel1081610816 <niel10816albaredo3@gmail.com>
2026-02-12 17:01:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 import streamlit as st
 from openai import OpenAI
 
-client = OpenAI(api_key="sk-or-v1-37c...9a8", base_url="httpsw://openrouter.ai/api/v1")
+client = OpenAI(
+    api_key="sk-or-v1-37c...9a8",
+    base_url="https://openrouter.ai/api/v1")
 
-st.title("Consigliatore Film/Serie")
+st.title("Consigliatore Film/Serie 🎬")
 
-titolo = st.text_input("Scrivi un film o una serie che ti è piaciuto")
+film = st.text_input("Scrivi un film o serie che ti è piaciuto:")
 
-if st.button("Trova qualcosa di simile"):
-    if titolo:
-        risposta = client.chat.completions.create(model="opena/gpt-4o-mini",messages=[{"role": "system", "content": "sei un assistente che consiglia film o seriesimili a quello indicato, basandotisullo stesso mood e genere."},
-                                                                                      {"role": "user", "content": titolo}])
-testo = risposta.choices[0].message["content"]
-st.write(testo)
+if st.button("Trova qualcosa con lo stesso mood"):
+    if film:
+        risposta = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[{"role": "system", "content": "Consiglia un film o serie con lo stesso mood."},{"role": "user", "content": film}])
 
+        testo = risposta.choices[0].message["content"]
+        st.write(testo)
+
+    else:
+        st.warning("Scrivi prima un film o serie.")
 
 
