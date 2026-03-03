036072aa758d3b3ffa09c19116f3b157fccf4c9a
niel1081610816 <niel10816albaredo3@gmail.com>
2026-03-03 18:32:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from supabase import create_client
 url = "https://kjqncgasirjzilovibat.supabase.co"
 key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtqcW5jZ2FzaXJqemlsb3ZpYmF0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0NjkyNTgsImV4cCI6MjA4ODA0NTI1OH0.qDVawy4rq8QCO9fP5tJsTSVN8PrgZW5BKwFOWxX2ECI"
 supabase = create_client(url,key)
-st.title("Titolo del Sito")
+st.title("BeginnerCollab")
 nome = st.text_input("Email *questa email sarà visibile a tutti gli utenti della piattaforma")
 ruolo = st.selectbox("Chi sei?",["produttore", "cantante", "entrambi"])
 genere = st.selectbox("genere", ["rock", "rap/trap", "pop", "classico", "tecno/elettro", "jazz"])
@@ -50,6 +50,7 @@ if data and len(data) > 1:
     for u, score in risultati:
         if u["nome"] != current_user["nome"]:
             st.write(f"{u['nome']} → compatibilità: {score}%")
+
 
 
 
