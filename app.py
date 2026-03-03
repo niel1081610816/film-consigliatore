203d028d8c56ebe18b180f1d26dd83fc3cc3a2fd
niel1081610816 <niel10816albaredo3@gmail.com>
2026-03-03 17:24:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from supabase import create_client
 url = "https://kjqncgasirjzilovibat.supabase.co"
 key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtqcW5jZ2FzaXJqemlsb3ZpYmF0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0NjkyNTgsImV4cCI6MjA4ODA0NTI1OH0.qDVawy4rq8QCO9fP5tJsTSVN8PrgZW5BKwFOWxX2ECI"
-
+supabase = client_(url,key)
 if st.button("Salva il profilo"):
     supabase.table("utenti").insert({
         "nome": nome,
@@ -27,6 +27,7 @@ if data and len(data) > 1:
     for u, score in risultati:
         if u["nome"] != current_user["nome"]:
             st.write(f"{u['nome']} → compatibilità: {score}%")
+
 
 
 
