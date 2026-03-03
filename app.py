a06d5d54d65aafcbe265576e2c8a91cb92a263e5
niel1081610816 <niel10816albaredo3@gmail.com>
2026-03-03 16:54:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from supabase import create_client
-url = "https://jnaujwpfuhynogktbfua.supabase.co"
-key= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpuYXVqd3BmdWh5bm9na3RiZnVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0NjkyOTksImV4cCI6MjA4ODA0NTI5OX0.X_PFJHONaGMYqAJG-5Br7iE4_xsv-umiFcLyl-JiMuw"
+url = "https://kjqncgasirjzilovibat.supabase.co"
+key= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtqcW5jZ2FzaXJqemlsb3ZpYmF0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0NjkyNTgsImV4cCI6MjA4ODA0NTI1OH0.qDVawy4rq8QCO9fP5tJsTSVN8PrgZW5BKwFOWxX2ECI"
 supabase = create_client(url, key)
 st.title("Titolo del sito")
 nome = st.text_input("Nome")
@@ -51,6 +51,7 @@ if response.data:
 
 
     if u["nome"] != current_user["nome"]:st.write(f"{u['nome']} ➝‬ compatibilità: {score}%")
+
 
 
 
